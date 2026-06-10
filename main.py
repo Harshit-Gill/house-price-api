6b22dbfc062b7f5bbd4f93254706fa08f54c79a6
Harshit Gill <harshit25csu082@ncuindia.edu>
2026-06-11 04:36:21 +0530

    add CORS middleware

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import pandas as pd
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 pipe = joblib.load("house_price_pipeline.pkl")
 
 class HouseFeatures(BaseModel):
